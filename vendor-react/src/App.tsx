31bdacbeed6b600b1c107903b7a3909e61ab8050
Chinmay <chinmay.singh@shipglobal.in>
2024-12-10 11:10:07 +0530

    cleaned vendor code

--- vendor-react/src/App.tsx
+++ vendor-react/src/App.tsx
@@ -3,7 +3,6 @@ import PublicRoutes from "./PublicRoutes";
 
 function App() {
   const router = createBrowserRouter([PublicRoutes()]);
-  console.log(import.meta.env);
   return <RouterProvider router={router} />;
 }
 
