d9b42d94a5a29c32e358b998b9b18d59b8564d8c
Chinmay <chinmay.singh@shipglobal.in>
2024-12-10 14:19:20 +0530

    added login page

--- franchise-react/src/pages/auth/Login.tsx
+++ franchise-react/src/pages/auth/Login.tsx
@@ -5,17 +5,15 @@ const Card = lazy(() => import("@/components/ui/card"));
 const PublicPages = lazy(() => import("@/layouts/PublicPages"));
 const EmailLoginForm = lazy(() => import("@/components/templates/forms/EmailLogin"));
 
-export default function Login() {
+export default function Login({ title, children }: { title?: string; children?: any }) {
   return (
     <PublicPages>
       <div className="flex flex-col items-center justify-center m-4">
         <Card className="w-full max-w-md mb-18 lg:mb-32 h-128">
           <CardHeader>
-            <CardTitle className="text-xl text-center">Login</CardTitle>
+            <CardTitle className="text-xl text-center">{title ? title : "Login"}</CardTitle>
           </CardHeader>
-          <CardContent>
-            <EmailLoginForm />
-          </CardContent>
+          <CardContent>{children ? children : <EmailLoginForm />}</CardContent>
         </Card>
       </div>
     </PublicPages>
--- vendor-react/src/pages/auth/Login.tsx
+++
@@ -1,7 +0,0 @@
-import { lazy } from "react";
-
-const LoginPage = lazy(() => import("franchise/Login"));
-
-export default function Login() {
-  return <LoginPage />;
-}
